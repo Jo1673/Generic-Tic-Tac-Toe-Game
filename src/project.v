93bbd145a7bfa7c97fbe9b80d9da4835f9a78235
Joaquin Guzman <Joa513quin@gmail.com>
2026-05-18 18:26:02 -0400

    ran tests

--- src/project.v
+++ src/project.v
@@ -7,7 +7,7 @@
 
 `include "hvsync_generator.v"
 
-module tt_um_Joa1673 (
+module tt_um_Jo1673 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
